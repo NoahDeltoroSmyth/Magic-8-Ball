e12085e8ca8b933d462f6a30c20cb57eb9d1d4f7
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-10-05 14:40:08 -0700

    click event works and cycles through answer array now

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import { answers } from './answers.js';
 
-const userInput = document.getElementById('user-input');
+//const userInput = document.getElementById('user-input');
 const Button = document.getElementById('answer-button');
 const answerMessage = document.getElementById('answer-message');
 
@@ -8,22 +8,9 @@ const getRandomNumber = (max)=>{
     return Math.floor(Math.random() * max);
 };
 
-const randomIndex = getRandomNumber(answers.length);
-
-const randomAnswer = getRandomNumber(answers.length);
-
-
-
 Button.addEventListener('click', ()=>{
-  const randomIndex = getRandomNumber(0, answers.length);
-  const randomAnswer = answers[randomIndex];
-  answerMessage.textContent = randomAnswer;
-  
+    const randomIndex = getRandomNumber(answers.length);
+    const randomAnswer = answers[randomIndex];
+    answerMessage.textContent = randomAnswer;
     
 });
-
-
-
-  //to generate a random color from the array
-  //generate a random number between 0 and 2
-  //in other words generate a random index
