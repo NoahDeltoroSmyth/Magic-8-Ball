7193dd3cd2dc75ed8c0bac56da078f0d8a7e4303
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-10-05 14:23:17 -0700

    add mathrandom and variables for index and answers and added them to buttoneventlistener

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
 import { answers } from './answers.js';
 
-const userQuestion = document.getElementById('user-question');
+const userInput = document.getElementById('user-input');
 const Button = document.getElementById('answer-button');
 const answerMessage = document.getElementById('answer-message');
 
+const getRandomNumber = (max)=>{
+    return Math.floor(Math.random() * max);
+};
+
+const randomIndex = getRandomNumber(answers.length);
+
+const randomAnswer = getRandomNumber(answers.length);
 
 
 
 Button.addEventListener('click', ()=>{
-
+  const randomIndex = getRandomNumber(0, answers.length);
+  const randomAnswer = answers[randomIndex];
+  answerMessage.textContent = randomAnswer;
+  
+    
 });
 
-const colors = ['red', 'white', 'blue'];
 
 
   //to generate a random color from the array
   //generate a random number between 0 and 2
   //in other words generate a random index
-const getRandomNumber = (max)=>{
-    return Math.floor(Math.random() * max);
-}
-const randomIdx = getRandomNumber(colors.length);
-console.log(colors[randomIdx]);
-
-const randomAnswer = getRandomNumber(answers.length);
-console.log(answers[randomAnswer]);
